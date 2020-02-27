225c9c92d01d94fdc6ba834328e0882b57584a5d
Lincoln-Brown <links1155@gmail.com>
2020-02-27 05:35:20 -0500

    step10

--- app/views.py
+++ app/views.py
@@ -28,6 +28,12 @@ def about():
     """Render the website's about page."""
     return render_template('about.html')
 
+@app.route('/secure-page')
+@login_required 
+def secure_page():
+
+
+    return render_template("secure_page.html")
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
@@ -39,9 +45,6 @@ def login():
             # Get the username and password values from the form.
             username = form.username.data
             password = form.password.data
-
-
-
             # using your model, query database for a user based on the username
             # and password submitted. Remember you need to compare the password hash.
             # You will need to import the appropriate function to do so.
@@ -52,15 +55,23 @@ def login():
                 remember_me = False
                 # get user id, load into session
                 login_user(user,remember_me)
-                print("userlogded in")
-                flash('Logged in successfully.', 'success')
+                flash('Login successful.', 'success')
                 # remember to flash a message to the user
-                return redirect(url_for("home"))
+                return redirect(url_for("secure_page"))
                 # they should be redirected to a secure-page route instead
             else:
                  flash('Username or Password is incorrect.', 'danger')
 
     return render_template("login.html", form=form)
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    flash('You have been logged out.', 'danger')
+    return redirect(url_for('home'))
+
+
 
 
 # user_loader callback. This callback is used to reload the user object from
